0b170f8a5ad80f106ba7a6d8f5cb7d08d517644e
fbackhed <47496572+fbackhed@users.noreply.github.com>
2022-05-03 20:22:37 +0200

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,10 @@ import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const [partyTime, setPartyTime] = useState(false);
-  const [days, setDays] = useState(0);
-  const [hours, setHours] = useState(0);
-  const [minutes, setMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(0);
+  const [days, setDays] = useState('');
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [seconds, setSeconds] = useState('');
 
   useEffect(() => {
     const target = new Date("12/31/2022 23:59:59");
